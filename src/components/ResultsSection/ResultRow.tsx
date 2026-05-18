5ab24b418203a548b06168197f0c33797b21c1d8
wrknbuycnsmndie <a_chapala@mail.ru>
2026-05-18 15:11:20 +0300

    refactor(components): convert Header and results UI components to functional style

--- src/components/ResultsSection/ResultRow.tsx
+++ src/components/ResultsSection/ResultRow.tsx
@@ -1,19 +1,14 @@
-import { Component } from 'react';
 import type { SearchResultItem } from '../../types/search';
 
 type ResultRowProps = {
     item: SearchResultItem;
 };
 
-export class ResultRow extends Component<ResultRowProps> {
-    public render() {
-        const { item } = this.props;
-
-        return (
-            <li className="results-section__result" aria-label={item.name}>
-                <span className="results-section__result-name">{item.name}</span>
-                <span className="results-section__result-description">{item.description}</span>
-            </li>
-        );
-    }
+export function ResultRow({ item }: ResultRowProps) {
+    return (
+        <li className="results-section__result" aria-label={item.name}>
+            <span className="results-section__result-name">{item.name}</span>
+            <span className="results-section__result-description">{item.description}</span>
+        </li>
+    );
 }
