dd60f26b9e50576091faf69f20a98d58f0445425
wrknbuycnsmndie <a_chapala@mail.ru>
2026-05-18 15:23:46 +0300

    refactor(app): convert App component to functional style

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,34 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 import { Header } from './components/Header/Header';
 import { PokemonSearch } from './components/PokemonSearch/PokemonSearch';
 import './App.css';
 
-type AppState = {
-    shouldThrowError: boolean;
-};
+export default function App() {
+    const [shouldThrowError, setShouldThrowError] = useState(false);
 
-export default class App extends Component<object, AppState> {
-    public state: AppState = {
-        shouldThrowError: false,
+    const handleResetError = () => {
+        setShouldThrowError(false);
     };
 
-    private handleResetError = () => {
-        this.setState({ shouldThrowError: false });
+    const handleTriggerError = () => {
+        setShouldThrowError(true);
     };
 
-    private handleTriggerError = () => {
-        this.setState({ shouldThrowError: true });
-    };
+    return (
+        <main className="app">
+            <div className="app__container">
+                <ErrorBoundary onReset={handleResetError}>
+                    <Header />
 
-    public render() {
-        const { shouldThrowError } = this.state;
-
-        return (
-            <main className="app">
-                <div className="app__container">
-                    <ErrorBoundary onReset={this.handleResetError}>
-                        <Header />
-
-                        <div className="app__content">
-                            <PokemonSearch
-                                onTestError={this.handleTriggerError}
-                                shouldThrowError={shouldThrowError}
-                            />
-                        </div>
-                    </ErrorBoundary>
-                </div>
-            </main>
-        );
-    }
+                    <div className="app__content">
+                        <PokemonSearch
+                            onTestError={handleTriggerError}
+                            shouldThrowError={shouldThrowError}
+                        />
+                    </div>
+                </ErrorBoundary>
+            </div>
+        </main>
+    );
 }
